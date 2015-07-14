78ce6f665cded9c8eeba43d12ebcefb03677206f
Guy Thomas <guy.r.thomas@gmail.com>
2015-07-14 15:51:03 -0700

    Update AS5048A.cpp

--- AS5048A.cpp
+++ AS5048A.cpp
@@ -1,1 +1,28 @@
+/*
 
+
+*/
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include "AS5048.h"
+#include "SPI.h"
+
+//#define AS5048_DEBUG
+
+/*
+  Constructor for single sensor SPI
+*/
+AS5048::AS5048(byte ar_cableSelect){
+  _cableSelect = art_cableSelect);
+  errorFlag = false;
+  position = 0;
+  pinMode(_cableSelect, OUTPUT);
+}
+
+AS5048::AS5048(byte arg_cableSelect, byte arg_numSensors){
+
+}
