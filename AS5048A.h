97978fd27945696a9f911a6ed18cd769d2aa8a11
Guy Thomas <guy.r.thomas@gmail.com>
2015-07-14 09:47:37 -0700

    Update AS5048A.h

--- AS5048A.h
+++ AS5048A.h
@@ -1,1 +1,18 @@
+/*
+AS5048.h - Library for interacting with the AS5048A magnetic angular position sensors
+http://ams.com/eng/Products/Position-Sensors/Magnetic-Rotary-Position-Sensors/AS5048A
+Created by Guy Thomas, July 2015
 
+*/
+#ifndef AS5048_h
+#define AS5048_h
+
+#include "Arduino.h"
+
+class AS5048
+{
+  Public:
+  Private:
+  bit _Parity;
+}
+#endif
